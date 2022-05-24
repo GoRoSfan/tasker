097202a2eccdcdcefaca0ea1b5d3d994b890bf3c
GoRoS <olsngrs@gmail.com>
2022-05-24 19:54:03 +0300

    init project

---
+++ board/models.py
@@ -0,0 +1,67 @@
+from django.db import models
+
+# Create your models here.
+
+class Point(models.Model):
+
+    name = models.CharField(max_length=200, verbose_name=_('Name'))
+    description = models.TextField(verbose_name=_('Description'))
+
+    latitude = models.DecimalField(max_digits=9, decimal_places=6,
+                                   verbose_name=_('Latitude'))
+    longitude = models.DecimalField(max_digits=9, decimal_places=6,
+                                    verbose_name=_('Longitude'))
+
+    city = models.ForeignKey('City', on_delete=models.SET_NULL, null=True,
+                             related_name='points', verbose_name=_('City'))
+
+    class Meta:
+        verbose_name = _('Point')
+        verbose_name_plural = _('Points')
+        unique_together = ('name', 'city')
+
+    def __str__(self):
+        return '{} ({})'.format(self.name, self.city.name)
+
+    def __repr__(self):
+        return 'Point({}, {}, {}, {})'.format(
+            self.name, self.latitude, self.longitude, self.city.name)
+
+
+class PointImage(models.Model):
+    point = models.ForeignKey(Point, on_delete=models.CASCADE,
+                              related_name='images', verbose_name=_('Point'))
+
+    url = models.URLField(verbose_name='URL')
+
+    class Meta:
+        verbose_name = _('Point image')
+        verbose_name_plural = _('Point images')
+
+    def __str__(self):
+        return str(self.pk)
+
+    def __repr__(self):
+        return 'PointImage({}, {})'.format(self.point.name, self.url)
+
+
+class City(models.Model):
+
+    name = models.CharField(max_length=100, verbose_name=_('Name'))
+
+    latitude = models.DecimalField(max_digits=9, decimal_places=6,
+                                   verbose_name=_('Latitude'))
+    longitude = models.DecimalField(max_digits=9, decimal_places=6,
+                                    verbose_name=_('Longitude'))
+
+    class Meta:
+        verbose_name = _('City')
+        verbose_name_plural = _('Cities')
+
+    def __str__(self):
+        return self.name
+
+    def __repr__(self):
+        return 'City({}, {}, {})'.format(self.name, self.latitude,
+                                         self.longitude)
+
---
+++ tasker/celery.py
@@ -0,0 +1,23 @@
+"""
+https://docs.celeryq.dev/en/stable/django/first-steps-with-django.html
+"""
+import os
+
+from celery import Celery
+
+from django.conf import settings
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tasker.settings')
+
+app = Celery("tasker")
+
+app.config_from_object('django.conf:settings', namespace='CELERY')
+
+app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
+
+
+@app.task
+def divide(x, y):
+    import time
+    time.sleep(5)
+    return x / y
